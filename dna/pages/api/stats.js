5a94555654c4368a53c4bb545db4b571c02022e7
RamsPantoja <worddraco1@gmail.com>
2021-06-21 14:05:22 -0500

    Se resteo la funcion en api/stats y se modifico la variable MONGO_URI.

--- dna/pages/api/stats.js
+++ dna/pages/api/stats.js
@@ -1,18 +1,22 @@
 import dbConnect from "../../lib/dbConnect";
 import Dna from "../../lib/dna";
 
-export default async function getStats(req, res) {
+const getStats = (req, res) =>  {
+    if (req.method === 'GET') {
         await dbConnect();
 
         const count_mutations = await Dna.find({hasMutation: true});
         const count_no_mutation = await Dna.find({hasMutation: false});
         const ratio = count_mutations.length/count_no_mutation.length;
 
-        stats = {
+        const stats = {
             count_mutations: count_mutations.length,
             count_no_mutation: count_no_mutation.length,
             ratio: ratio,
         }
 
         return res.status(200).send(stats); 
+    }
 }
+
+export default getStats;
