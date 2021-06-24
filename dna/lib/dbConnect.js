33920f7cb07972739505791dd54ef720a7571261
RamsPantoja <worddraco1@gmail.com>
2021-06-24 01:21:29 -0500

    Se agrego un custom server

--- dna/lib/dbConnect.js
+++ dna/lib/dbConnect.js
@@ -5,7 +5,7 @@ const dbConnect = async () => {
         return;
     }
 
-    return mongoose.connect(process.env.MONGO_URI, {
+    return mongoose.connect(process.varibles.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
